56dcc6533482fcc2b62b36bf672c8db956bd0236
kshitij_joshi <kshitijhcpj@gmail.com>
2026-07-13 01:28:33 +0530

    Time: 16 ms (99.05%), Space: 209.2 MB (95.45%) - LeetHub

---
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+vector<int> rank,parent;
+void disjoint(int n){
+rank.resize(n+1,0);
+parent.resize(n+1);
+for(int i=1;i<=n;i++){
+    parent[i]=i;
+}
+}
+int findPar(int node){
+    if(node==parent[node]){
+        return node;
+    }
+    return parent[node]=findPar(parent[node]);
+}
+void rankUnion(int u,int v){
+    int ul_u=findPar(u);
+    int ul_v=findPar(v);
+    if(ul_u==ul_v){
+        return;
+    }
+    if(rank[ul_u]>rank[ul_v]){
+        parent[ul_v]=ul_u;
+    }
+    if(rank[ul_v]>rank[ul_u]){
+        parent[ul_u]=ul_v;
+    }
+    else{
+        parent[ul_v]=ul_u;
+        rank[ul_u]++;
+    }
+}
+    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
+      disjoint(n);
+
+      for(auto& edge:edges){
+        int u=edge[0];
+        int v=edge[1];
+
+        rankUnion(u,v);
+        
+      }
+
+      if(findPar(source)==findPar(destination)){
+        return true;
+      }
+      return false;
+
+    }
+};
