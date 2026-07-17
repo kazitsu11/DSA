1605479e3d6b53b47526e4863cfcadf1642f0649
kshitij_joshi <kshitijhcpj@gmail.com>
2026-07-17 20:29:42 +0530

    Time: 259 ms (12.31%), Space: 305.2 MB (5.03%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    long long ks(vector<int>& arr, int target, int n, vector<vector<long long>>& dp) {
+
+        if (target == 0) {
+            return 0;
+        }
+        if (target == 1)
+            return 1;
+        if (n == 0) {
+            return 1e9;
+        }
+        if (dp[n][target] != 1e9) {
+            return dp[n][target];
+        }
+        if (arr[n - 1] <= target) {
+            return dp[n][target] = min(1 + ks(arr, target - arr[n - 1], n, dp),
+                                       ks(arr, target, n - 1, dp));
+        } else if (arr[n - 1] > target) {
+            return dp[n][target] = ks(arr, target, n - 1, dp);
+        }
+        return dp[n][target];
+    }
+    int numSquares(int target) {
+        vector<int> arr;
+
+        for (int i = 1; i*i <= target; ++i) {
+            arr.push_back(i * i);
+        }
+        int n = arr.size();
+        vector<vector<long long>> dp(n + 1, vector<long long>(target + 1, 1e9));
+
+        return ks(arr, target, n, dp);
+    }
+};
