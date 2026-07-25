b189a6e72509deaeadb25af1742a7e1e8efdaec5
kshitij_joshi <kshitijhcpj@gmail.com>
2026-07-26 02:59:58 +0530

    Time: 24 ms (80.52%), Space: 17.5 MB (36.42%) - LeetHub

---
+++ 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int findNumberOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n, 1);
+        vector<int>count(n,1);
+        int maxlis = 1;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (nums[i] > nums[j]) {
+                    if(dp[j]+1>dp[i]){
+                     dp[i]=dp[j]+1;
+                     count[i]=count[j];
+                    }
+                    else if(dp[i]==dp[j]+1){
+                        count[i]+=count[j];
+                    }
+                }
+            }
+             maxlis=max(maxlis,dp[i]);
+        }
+
+        int ans=0;
+        for(int i=0;i<n;++i){
+            if(dp[i]==maxlis){
+                ans+=count[i];
+            }
+        }
+        return ans;
+    }
+};
