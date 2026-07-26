50271f141c30d55380b5fedf444ef60c330c75ad
kshitij_joshi <kshitijhcpj@gmail.com>
2026-07-26 20:58:06 +0530

    Time: 0 ms (100%), Space: 8.7 MB (20.47%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int>dp(n+1);
+
+        for(int i=1;i<n+1;++i){
+            if(i==1 || i==2){
+              dp[i]=i;
+            }
+            else
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
+};
