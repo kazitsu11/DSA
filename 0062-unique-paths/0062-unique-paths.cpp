cc16bcbc02d6b634278d399903d05a5affdbc807
kshitij_joshi <kshitijhcpj@gmail.com>
2026-07-29 02:54:22 +0530

    Time: 0 ms (100%), Space: 7.9 MB (93.92%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
-    vector<vector<int>> directions = {{1, 0}, {0, 1}};
-    int dfs(int i, int j, int n, int m, vector<vector<int>>& dp) {
-        if (i == n - 1 && j == m - 1) {
-            return 1;
-        }
-        if (dp[i][j] != -1) {
+    int dp[101][101];
+    int solve(int i, int j) {
+        if (i < 0 || j < 0)  return 0
+            ;
+
+        if (i == 0 && j == 0) return 1;
+
+        if(dp[i][j]!=-1){
             return dp[i][j];
         }
-        int count = 0;
+            
 
-        for (auto& dir : directions) {
-            int new_i = i + dir[0];
-            int new_j = j + dir[1];
+        int up = solve(i - 1, j);
+        int left = solve(i, j - 1);
 
-            if (new_i >= 0 && new_j >= 0 & new_i < n && new_j < m) {
-                count += dfs(new_i, new_j, n, m, dp);
-            }
-        }
-        return dp[i][j] = count;
+        return dp[i][j]=up + left;
     }
-    int uniquePaths(int n, int m) {
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
-        return dfs(0, 0, n, m, dp);
+    int uniquePaths(int m, int n) {
+        memset(dp, -1, sizeof(dp));
+        return solve(m - 1, n - 1);
     }
 };
