5a9cf13766a2bff763d72fd7f07c274f945e22d9
kshitij_joshi <kshitijhcpj@gmail.com>
2026-07-28 19:06:37 +0530

    Time: 0 ms (100%), Space: 9.5 MB (5.32%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> directions = {{1, 0}, {0, 1}};
+    int dfs(int i, int j, int n, int m, vector<vector<int>>& dp) {
+        if (i == n - 1 && j == m - 1) {
+            return 1;
+        }
+        if (dp[i][j] != 0) {
+            return dp[i][j];
+        }
+        int count = 0;
+
+        for (auto& dir : directions) {
+            int new_i = i + dir[0];
+            int new_j = j + dir[1];
+
+            if (new_i >= 0 && new_j >= 0 & new_i < n && new_j < m) {
+                count += dfs(new_i, new_j, n, m, dp);
+            }
+        }
+        return dp[i][j] = count;
+    }
+    int uniquePaths(int n, int m) {
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+        return dfs(0, 0, n, m, dp);
+    }
+};
