53f7f46f6adbca1530ffce167a4c54cd941331b9
kshitij_joshi <kshitijhcpj@gmail.com>
2026-07-30 02:00:29 +0530

    Time: 0 ms (100%), Space: 9.2 MB (75.11%) - LeetHub

---
+++ 0980-unique-paths-iii/0980-unique-paths-iii.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+int res=0,empty=1;
+void dfs(vector<vector<int>>& grid,int i,int j,int count){
+    if(i<0 || i>=grid.size() || j<0 ||j>=grid[0].size()||grid[i][j]==-1) return;
+
+    if(grid[i][j]==2){
+        if(empty==count) res++;
+        return;
+    }
+    grid[i][j]=-1;
+   
+    dfs(grid,i+1,j,count+1);
+    dfs(grid,i-1,j,count+1);
+    dfs(grid,i,j-1,count+1);
+    dfs(grid,i,j+1,count+1);
+
+    grid[i][j]=0;
+}
+    int uniquePathsIII(vector<vector<int>>& grid) {
+     int n=grid.size();
+     int m=grid[0].size();
+     int start_x,start_y;
+
+     for(int i=0;i<n;++i){
+        for(int j=0;j<m;++j){
+            if(grid[i][j]==1){
+               start_x=i;
+               start_y=j;
+            }
+            else if(grid[i][j]==0)
+                empty++;
+            }
+        }
+    dfs(grid,start_x,start_y,0); 
+    return res;  
+    }
+};
