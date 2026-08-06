88f3fbf781c4b59a9746f6017400fe7f6957e3ee
kshitij_joshi <kshitijhcpj@gmail.com>
2026-08-06 11:52:46 +0530

    Time: 0 ms (100%), Space: 8.5 MB (97.62%) - LeetHub

--- 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.cpp
+++ 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.cpp
@@ -1,35 +1,30 @@
 class Solution {
 public:
-    bool isDiv(int mid, int t) {
-        // vector<int> ans;
-        int prod = 1;
-        int temp = mid;
-        while (temp > 0) {
-            int digit = temp % 10;
-            //ans.push_back(digit);
-            prod*=digit;
-            temp = temp / 10;
-        }
-        if (prod % t == 0) {
-            return true;
-        }
-        return false;
+bool isDiv(int n,int t){
+    int temp=n;
+    int prod=1;
+    while(temp>0){
+        int digit=temp%10;
+        prod*=digit;
+        temp=temp/10;
     }
+    if(prod%t==0){
+        return true;
+    }
+    return false;
+}
     int smallestNumber(int n, int t) {
+        
+        int k=1;
 
-        int low = n;
-        int high = n + t;
-        int ans;
-
-        while (low <= high) {
-            int mid = low;
-            if (isDiv(mid,t)) {
-                 ans = mid;
-                high=mid-1;
-            } else {
-                low = mid+1;
+        while(k>0){
+            if(isDiv(n,t)){
+                k--;
+            }
+            else{
+                n++;
             }
         }
-        return ans;
+        return n;
     }
 };
