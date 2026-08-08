8e92425c8b54a71699f0a541e4caff2ad5de16fe
kshitij_joshi <kshitijhcpj@gmail.com>
2026-08-09 00:00:27 +0530

    Time: 61 ms (10.64%), Space: 115.5 MB (24.47%) - LeetHub

--- 3302-find-the-lexicographically-smallest-valid-sequence/3302-find-the-lexicographically-smallest-valid-sequence.cpp
+++ 3302-find-the-lexicographically-smallest-valid-sequence/3302-find-the-lexicographically-smallest-valid-sequence.cpp
@@ -4,39 +4,38 @@ public:
         int n=word1.size();
         int m=word2.size();
 
-        vector<int>rightMatch(n,0);
-
         int i=n-1;
         int j=m-1;
+        vector<int>rightmatch(n,0);
         int match=0;
 
         while(i>=0){
-            if(j>=0 && word1[i]==word2[j]){
-             match++;
-             j--;
+            if(j>=0 && word1[i]==word2[j] ){
+              match++;
+              j--;
             }
-            rightMatch[i]=match;
+            rightmatch[i]=match;
             i--;
         }
 
-        vector<int>seq;
+         i=0;
+         j=0;
+        vector<int>ans;
         bool canChange=true;
 
-       i=0;
-       j=0;
         while(i<n && j<m){
             if(word1[i]==word2[j]){
-                seq.push_back(i);
-                j++;
+               ans.push_back(i);
+               j++;
             }
-           else if(canChange==true && i+1<n && rightMatch[i+1]>=m-j-1){
-                seq.push_back(i);
+            else if(canChange==true && i+1<n && rightmatch[i+1]>=m-j-1 ){
+                ans.push_back(i);
                 canChange=false;
                 j++;
             }
-            i++;
+            ++i;
         }
-        if(j==m) return seq;
+        if(j==m) return ans;
         return {};
     }
 };
