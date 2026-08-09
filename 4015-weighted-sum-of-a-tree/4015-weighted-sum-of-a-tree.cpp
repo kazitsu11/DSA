3ab7d34aa0834eaeca6f12fad07c3e2b58d8588c
kshitij_joshi <kshitijhcpj@gmail.com>
2026-08-09 10:51:21 +0530

    Time: 141 ms (63.4%), Space: 244.2 MB (31.17%) - LeetHub

---
+++ 4015-weighted-sum-of-a-tree/4015-weighted-sum-of-a-tree.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int height;
+    void dfs(int root, vector<vector<int>>& adj, int d, vector<int>& depth) {
+        depth[root] = d;
+        height = max(height, d);
+
+        for (auto& neigh : adj[root]) {
+            dfs(neigh, adj, d + 1, depth);
+        }
+    }
+    long long weightedSum(vector<int>& parent, vector<int>& nums) {
+        //sort(nums.begin(), nums.end());
+        int n = parent.size();
+        int m = nums.size();
+
+        vector<vector<int>> adj(n);
+        for (int i = 0; i < n; ++i) {
+            if (parent[i] != -1) {
+            adj[parent[i]].push_back(i);
+        }
+    }
+    vector<int> depth(n);
+    dfs(0, adj, 1, depth);
+
+    long long weight = 0;
+
+    for (int i = 0; i < n; ++i) {
+        weight += 1LL *nums[i] * (height - depth[i] + 1);
+    }
+    return weight;
+}
+}
+;
