b1791d8223f7adac53f12ed187c58e58ee4fda0d
kshitij_joshi <kshitijhcpj@gmail.com>
2026-08-10 09:54:27 +0530

    Time: 67 ms (33.87%), Space: 21.3 MB (14.21%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.cpp
+++ 1510-stone-game-iv/1510-stone-game-iv.cpp
@@ -1,28 +1,17 @@
 class Solution {
 public:
-    bool solve(int alice, int bob, vector<int>& arr, int n,vector<int>&dp) {
+    bool solve( vector<int>& arr, int n,vector<int>&dp) {
         if (n == 0) return false;
         if(dp[n]!=-1) return dp[n];
-        if (alice == 1) {
             for (int k = 0; k < arr.size(); ++k) {
                 if (arr[k] <= n) {
-                    bool result = solve(0, 1, arr, n-arr[k],dp);
+                    bool result = solve( arr, n-arr[k],dp);
                     if (result == false) {
                         return dp[n]=true;
                     }
                 }
             }
-        } 
-        else if (bob == 1) {
-            for (int k = 0; k < arr.size(); ++k) {
-                if (arr[k] <= n) {
-                    bool result = solve(1, 0, arr, n-arr[k],dp);
-                    if (result == false) {
-                        return dp[n]=true;
-                    }
-                }
-            }
-        }
+        
         return dp[n]=false;
     }
      bool winnerSquareGame(int n) {
@@ -32,7 +21,7 @@ public:
         }
 
         vector<int>dp(n+1,-1);
-        return solve(1, 0, arr, n,dp);
+        return solve( arr, n,dp);
 
     }
 };
