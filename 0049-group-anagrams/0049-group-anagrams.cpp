2478550bec893437393bb5186a7f9a1416a7303c
kshitij_joshi <kshitijhcpj@gmail.com>
2026-08-18 00:18:44 +0530

    Time: 15 ms (69.38%), Space: 25.2 MB (63.07%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,67 +1,18 @@
 class Solution {
 public:
-
-    void solve(int i,
-               vector<string>& strs,
-               vector<string>& group,
-               vector<vector<string>>& ans,
-               vector<array<int,26>>& freq,
-               vector<bool>& vis) {
-
-        int n = strs.size();
-
-        // Put current string in its group
-        group.push_back(strs[i]);
-        vis[i] = true;
-
-        // Find all anagrams
-        for(int j = i + 1; j < n; j++) {
-
-            if(vis[j])
-                continue;
-
-            if(freq[i] == freq[j]) {
-
-                group.push_back(strs[j]);
-                vis[j] = true;
-            }
-        }
-
-        ans.push_back(group);
-    }
-
-
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<string>>mp;
+        for(int i=0;i<strs.size();++i){
+            string temp=strs[i];
+            string org=temp;
 
-        int n = strs.size();
-
-        vector<vector<string>> ans;
-        vector<bool> vis(n, false);
-
-        // Calculate frequency of EVERY string only once
-        vector<array<int,26>> freq(n);
-
-        for(int i = 0; i < n; i++) {
-
-            freq[i].fill(0);
-
-            for(char c : strs[i]) {
-                freq[i][c - 'a']++;
-            }
+            sort(temp.begin(),temp.end());
+            mp[temp].push_back(org);
         }
-
-        // Your original idea:
-        // pick an unvisited string and find its anagrams
-        for(int i = 0; i < n; i++) {
-
-            if(vis[i])
-                continue;
-
-            vector<string> group;
-
-            solve(i, strs, group, ans, freq, vis);
+        vector<vector<string>>ans;
+        for(auto& a:mp){
+            ans.push_back(a.second);
         }
-
         return ans;
     }
 };
