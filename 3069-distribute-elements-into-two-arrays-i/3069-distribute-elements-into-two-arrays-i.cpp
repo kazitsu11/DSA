a0b3a64556891446c8cf85be06fbb9b8b122d6af
kshitij_joshi <kshitijhcpj@gmail.com>
2026-08-20 10:35:31 +0530

    Time: 3 ms (23.77%), Space: 24.1 MB (30.72%) - LeetHub

--- 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.cpp
+++ 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.cpp
@@ -20,17 +20,15 @@ public:
           }
         }
 
-        int m=arr1.size();
-        int j=arr2.size();
         vector<int>res;
 
-        for(int i=0;i<m;++i){
-            res.push_back(arr1[i]);
-        }
+     for(auto& a:arr1){
+        res.push_back(a);
+     }
 
-        for(int i=0;i<j;++i){
-            res.push_back(arr2[i]);
-        }
+     for(auto& a:arr2){
+        res.push_back(a);
+     }
 
         return res;
     }
