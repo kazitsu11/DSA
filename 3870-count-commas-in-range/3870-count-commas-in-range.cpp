7e69c72efedba91c717e25e312ac6c1d1b5b6890
kshitij_joshi <kshitijhcpj@gmail.com>
2026-09-08 11:15:13 +0530

    Time: 0 ms (100%), Space: 8.3 MB (99.47%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.cpp
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -1,22 +1,7 @@
 class Solution {
 public:
     int countCommas(int n) {
-        int size;
-        int curr = 1;
-        int comma=0;
-        while (curr <= n) {
-             size = 0;
-             int temp=curr;
-            while (temp > 0) {
-                int digit = temp % 10;
-                size++;
-                temp = temp / 10;
-            }
-            if (size > 3) {
-               comma+=(size-1)/3;
-            }
-          curr++;
-        }
-        return comma;
+        if(n>999) return n-999;
+        return 0;
     }
 };
