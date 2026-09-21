4a0351993371ba7ed04ed25826d79bdd9e976b22
kshitij_joshi <kshitijhcpj@gmail.com>
2026-09-21 19:47:52 +0530

    Time: 19 ms (97.83%), Space: 209 MB (96.8%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -1,51 +1,53 @@
 class Solution {
 public:
-vector<int> rank,parent;
-void disjoint(int n){
-rank.resize(n,0);
-parent.resize(n);
-for(int i=0;i<n;i++){
-    parent[i]=i;
-}
-}
-int findPar(int node){
-    if(node==parent[node]){
-        return node;
+    vector<int> rank, parent;
+    void disjoint(int n) {
+        rank.resize(n, 0);
+        parent.resize(n, 0);
+        for (int i = 0; i < n; ++i) {
+            parent[i] = i;
+        }
     }
-    return parent[node]=findPar(parent[node]);
-}
-void rankUnion(int u,int v){
-    int ul_u=findPar(u);
-    int ul_v=findPar(v);
-    if(ul_u==ul_v){
-        return;
-    }
-    if(rank[ul_u]>rank[ul_v]){
-        parent[ul_v]=ul_u;
-    }
-    if(rank[ul_v]>rank[ul_u]){
-        parent[ul_u]=ul_v;
-    }
-    else{
-        parent[ul_v]=ul_u;
-        rank[ul_u]++;
-    }
-}
-    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
-      disjoint(n);
 
-      for(auto& edge:edges){
-        int u=edge[0];
-        int v=edge[1];
+    int findPar(int node){
+        if(node==parent[node]){
+            return node;
+        }
+        return parent[node]=findPar(parent[node]);
+    }
 
-        rankUnion(u,v);
-        
-      }
+    void RankUnion(int u,int v){
+        int ul_u=findPar(u);
+        int ul_v=findPar(v);
 
-      if(findPar(source)==findPar(destination)){
-        return true;
-      }
-      return false;
+        if(ul_u==ul_v){
+            return;
+        }
+        if(rank[ul_u]>rank[ul_v]){
+            parent[ul_v]=ul_u;
+        }
+        else if(rank[ul_v]>rank[ul_u]){
+            parent[ul_u]=ul_v;
+        }
+        else{
+            parent[ul_v]=ul_u;
+            rank[ul_u]++;
+        }
+    }
 
+    bool validPath(int n, vector<vector<int>>& edges, int source,int destination) {
+        disjoint(n);
+
+        for(auto& e:edges){
+            int u=e[0];
+            int v=e[1];
+
+            RankUnion(u,v);
+        }
+
+        if(findPar(source)==findPar(destination)){
+            return true;
+        }
+        return false;
     }
 };
