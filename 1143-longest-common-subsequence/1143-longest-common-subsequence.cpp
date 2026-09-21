f540bbcd41915e8d97ddf459ada00f9345d5f20e
kshitij_joshi <kshitijhcpj@gmail.com>
2026-09-21 10:28:40 +0530

    Time: 23 ms (83.56%), Space: 27.5 MB (57.52%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -7,13 +7,13 @@ public:
 
         vector<vector<int>>dp(n+1,vector<int>(m+1,0));
 
-        for(int i=0;i<n+1;++i){
-            for(int j=0;j<m+1;++j){
-                if(i==0 || j==0){
-                    dp[i][j]=0;
-                }
-            }
-        }
+        // for(int i=0;i<n+1;++i){
+        //     for(int j=0;j<m+1;++j){
+        //         if(i==0 || j==0){
+        //             dp[i][j]=0;
+        //         }
+        //     }
+        // }
 
         for(int i=1;i<n+1;++i){
             for(int j=1;j<m+1;++j){
