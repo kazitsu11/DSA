d5a6f5e000ff4cfee7e1f3fa1346f37009976bff
kshitij_joshi <kshitijhcpj@gmail.com>
2026-09-23 19:16:48 +0530

    Time: 0 ms (100%), Space: 10.2 MB (47.03%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
@@ -1,43 +1,41 @@
 class MyQueue {
 public:
-    stack<int>input;
-    stack<int>output;
-    int peekEl=-1;
-    MyQueue() {
-        
-    }
-    
+    stack<int> input;
+    stack<int> output;
+    int peekEl = -1;
+    MyQueue() {}
+
     void push(int x) {
-        if(input.empty()){
-            peekEl=x;
+        if (input.empty()) {
+            peekEl = x;
         }
         input.push(x);
     }
-    
+
     int pop() {
-        if(output.empty()){
-            while(!input.empty()){
-                output.push(input.top());
-                input.pop();
-            }
+     if(output.empty()){
+        while(!input.empty()){
+            output.push(input.top());
+            input.pop();
         }
-         int val=output.top();
-            output.pop();
-            return val;
+     }
+     int val=output.top();
+     output.pop();
+     return val;
     }
-    
+
     int peek() {
-        if(output.empty()){
-            return peekEl;
-        }
-        return output.top();
+     if(output.empty()){
+        return peekEl;
+     }
+     return output.top();
     }
-    
+
     bool empty() {
-         if(input.empty() && output.empty()){
+        if(input.empty() && output.empty()){
             return true;
-         }
-         return false;
+        }
+        return false;
     }
 };
 
